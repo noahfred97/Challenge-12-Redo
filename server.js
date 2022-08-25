8fdb9893ead26b55805031ec085b4cd168ffa68a
noahfred97 <90789790+noahfred97@users.noreply.github.com>
2022-08-25 14:26:06 -0400

    5th

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 // Dependencies
 const inquirer = require("inquirer");
-const Util = reuqire ()
+const util = require ("./databasefiles/util.js");
+const queryUtil = require("./databasefiles/util.js");
+
+initialize();
+
+function initialize() {
+    inquirer
+    .createPromptModule({
+        type: "list",
+        choices: [
+            "New Department",
+            "New Employee",
+            "New Title",
+            "Display Departments",
+            "Display Employees",
+            "Display Title",
+            "Update Employee Title",
+            "Delete Title",
+            "Delete Employee",
+            "Delete Department",
+            "Exit",
+
+        ],
+        message: "How can I help you today?",
+        name: "choice",
+    })
+}
